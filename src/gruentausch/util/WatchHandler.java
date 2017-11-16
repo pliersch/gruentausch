3484d60dbfdc8cbd373f58fddad73c7910648add
pliersch <hourby@gmail.com>
2017-11-16 10:31:08 +0100

    add watchhandler

---
+++ src/gruentausch/util/WatchHandler.java
@@ -0,0 +1,59 @@
+package gruentausch.util;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.util.List;
+
+public class WatchHandler extends Thread {
+	WatchService watchService;
+	Path dirToWatch;
+	private Listener listener;
+
+	public interface Listener {
+		void handleFilesChanged();
+	}
+
+	public WatchHandler(WatchService watchService, Path dirToWatch, Listener listener) {
+		this.watchService = watchService;
+		this.dirToWatch = dirToWatch;
+		this.listener = listener;
+	}
+
+	@Override
+	public void run() {
+		System.out.println("run");
+		for (;;) {
+			try {
+				WatchKey key = watchService.take(); // blockiert
+				List<WatchEvent<?>> eventList = key.pollEvents();
+				System.out.println("size = " + eventList.size());
+				for (WatchEvent<?> e : eventList) {
+
+					if (e.kind() == StandardWatchEventKinds.ENTRY_CREATE) {
+						listener.handleFilesChanged();
+					}
+					System.out.print(e.kind() + " -> ");
+					Path name = (Path) e.context();
+					// System.out.print(name.getParent());
+					// context liefert nur den Dateinamen, parent ist null !
+					Path path = dirToWatch.resolve(name);
+					System.out.print(path);
+					if (Files.isDirectory(path))
+						System.out.println(" <dir>");
+					else
+						System.out.println(" <file<");
+				}
+				boolean valid = key.reset();
+				if (!valid) {
+					break;
+				}
+			} catch (InterruptedException ex) {
+				Logger.log(ex.getMessage());
+			}
+		}
+	}
+}
